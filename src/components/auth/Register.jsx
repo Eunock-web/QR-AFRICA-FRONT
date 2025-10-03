32dbe4d4334c2aa56c7533a574740a197db3c631
Eunock <izumishinishi7@gmail.com>
2025-10-03 12:42:01 +0200

    Correction

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -15,12 +15,12 @@ function Register() {
                     <div className="flex flex-row space-x-4 space-y-3">
                         <div className='w-1/2 text-blue-800 '>
                             <label htmlFor="prenom" className='text-blue-900 font-semibold'>Prénom</label>
-                            <Input type="text" placeholder="Jean" id="prenom" name="prenom" className = "focus:border-blue-500  w-full  h-10  shadow-2xl rounded-xl p-3 font-semibold" />
+                            <Input type="text" placeholder="Jean" id="prenom" name="prenom" className = "focus:border-blue-500  w-full  h-10  rounded-xl p-3 font-semibold" />
                         </div>
 
                         <div className='w-1/2  text-blue-800 '>
                             <label htmlFor="prenom" className='text-blue-900 font-semibold' >Nom</label>
-                            <Input type="text" placeholder="Dupont" id="nom" name="nom" className = " focus:ring-2 focus:ring-blue-500  w-full  h-10  shadow-2xl rounded-xl p-3 font-semibold-[10px] " />
+                            <Input type="text" placeholder="Dupont" id="nom" name="nom" className = " focus:ring-2 focus:ring-blue-500  w-full  h-10   rounded-xl p-3 font-semibold-[10px] " />
                         </div>
                     </div>
 
@@ -28,22 +28,22 @@ function Register() {
 
                         <div className=" flex flex-col space-y-2 ">
                                 <label htmlFor="email" className='text-blue-900 font-semibold' >Adresse email</label>
-                                <Input type="email" placeholder="votre@email.com" id ="email" name="email" className = "focus:border-blue-500 w-full  h-10   shadow-2xl rounded-xl p-3 font-semibold-[10px] " />
+                                <Input type="email" placeholder="votre@email.com" id ="email" name="email" className = "focus:border-blue-500 w-full  h-10    rounded-xl p-3 font-semibold-[10px] " />
                         </div>
 
                         <div className=" flex flex-col space-y-2 ">
                                 <label htmlFor="telephone" className='text-blue-900 font-semibold' >Numero  de téléphone</label>
-                                <Input type="text" placeholder="+33 45 25 45 78 08" id="telephone" name="telephone" className = "focus:border-blue-500 w-full  h-10  shadow-2xl rounded-xl p-3 font-semibold-[10px] " />
+                                <Input type="text" placeholder="+33 45 25 45 78 08" id="telephone" name="telephone" className = "focus:border-blue-500 w-full  h-10 rounded-xl p-3 font-semibold-[10px] " />
                         </div>
 
                         <div className=' flex flex-col space-y-3 '>
                                 <label htmlFor="password" className='text-blue-900 font-semibold' >Mot de passe</label>
-                                <Input type="password" placeholder=".........." id="password" name="password" className = "focus:border-blue-500 w-full  h-10  shadow-2xl rounded-xl p-3 font-semibold-[10px] " />
+                                <Input type="password" placeholder=".........." id="password" name="password" className = "focus:border-blue-500 w-full  h-10   rounded-xl p-3 font-semibold-[10px] " />
                         </div>
 
                         <div className=' flex flex-col space-y-3 ' >
                                 <label htmlFor="confirm_password" className='text-blue-900 font-semibold' >Confirmer le mot de passe</label>
-                                <Input type="password" placeholder=".........." id="confirm_password" name="password" className = "focus:border-blue-500 w-full  h-10   shadow-2xl rounded-xl p-3 font-semibold-[10px] " />
+                                <Input type="password" placeholder=".........." id="confirm_password" name="password" className = "focus:border-blue-500 w-full  h-10  rounded-xl p-3 font-semibold-[10px] " />
                         </div>
 
                         <div className='flex flex-row gap-2 space-y-3'>
@@ -57,8 +57,8 @@ function Register() {
                         </div>
 
                         <div className='flex flex-row justify-center gap-4'>
-                            <Button>Google</Button>
-                            <Button>facebook</Button>
+                            <Button className="rounded-xl ">Google</Button>
+                            <Button className="rounded-xl ">facebook</Button>
                         </div>
 
                         <div className=''>
