4a801b348f782897f5c90f468e729e8a1b9e37eb
Eunock-web <izumishinishi7@gmail.com>
2025-10-06 05:57:51 +0200

    Correction du chemain de Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-import Create from './components/pages/create'
+import Create from './components/pages/Create'
 import Lien from './components/pages/Lien'
 import Card from './components/utils/Card'
 import NavBar from './components/header/NavBar'
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="">
       <NavBar />
-      <Create />
+      {/* <Create /> */}
     </div>
   )
 }
